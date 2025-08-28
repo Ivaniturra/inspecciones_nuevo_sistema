c2e977793f8d7ae9010e3e43f0819791a7831847
Ivaniturra <162341297+Ivaniturra@users.noreply.github.com>
2025-08-28 15:05:51 -0400

    Update login.php

--- app/app/Views/auth/login.php
+++ app/app/Views/auth/login.php
@@ -95,8 +95,7 @@
 </head>
 <body>
   <?php
-  $logo = env('app.imagen_nomb_logo', 'logo.jpg');
-  $logo_path = env('app.ubicacion_logo_pagina', 'assets/img/');
+  
   ?>
 <?php echo base_url(); ?>
 <div class="auth-wrap">
@@ -109,11 +108,11 @@
     <div class="overlay"></div>
     <div class="inner">
       <div> 
-          <img src="<?= $logo; ?>"  style="height:48px" class="mb-3"> 
+          <img src="<?= env('app.imagen_nomb_logo') ?>"  style="height:48px" class="mb-3"> 
         <h1 class="brand-title"><?= env('app.title') ?></h1>
         <p class="mb-4 opacity-75">Bienvenido. Ingresa tus credenciales para continuar.</p>
         <!-- Imagen ilustrativa (cámbiala si quieres) -->
-        <img class="hero-img" src="<?= $logo_path; ?>"
+        <img class="hero-img" src="<?= env('app.imagen_nomb_logo2') ?>"
              alt="Ilustración de acceso">
       </div>
     </div>

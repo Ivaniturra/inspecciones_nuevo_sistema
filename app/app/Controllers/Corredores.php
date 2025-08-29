4096151798993c69efe91a82bb9c965d2e3df537
Ivaniturra <162341297+Ivaniturra@users.noreply.github.com>
2025-08-28 23:02:36 -0400

    Update Corredores.php

--- app/app/Controllers/Corredores.php
+++ app/app/Controllers/Corredores.php
@@ -333,7 +333,7 @@ class Corredores extends BaseController
             return redirect()->to('/corredores')->with('error', 'Corredor no encontrado');
         }
 
-        if (! $this->corredorModel->canDelete($id)) {
+        if (! $this->corredorModel->cascadeSetEnabled($id)) {
             return redirect()->to('/corredores')->with('error', 'No se puede eliminar el corredor porque tiene registros asociados');
         }
 

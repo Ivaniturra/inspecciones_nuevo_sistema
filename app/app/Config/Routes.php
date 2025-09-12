a309721ac0da6778ac2fef87c1dd480d6edbd28a
Ivaniturra <162341297+Ivaniturra@users.noreply.github.com>
2025-09-11 23:16:13 -0300

    Update Routes.php

--- app/app/Config/Routes.php
+++ app/app/Config/Routes.php
@@ -66,7 +66,7 @@ $routes->group('', ['filter' => 'auth'], static function($routes) {
         $routes->post('store', 'Corredor::store');
         $routes->get('show/(:num)', 'Corredor::show/$1');
         $routes->get('edit/(:num)', 'Corredor::edit/$1');
-        $routes->post('update/(:num)', 'Corredor::update/$1'); 
+        $routes->get('update/(:num)', 'Corredor::update/$1'); 
 
         $routes->get('delete/(:num)', 'Corredor::delete/$1');
         

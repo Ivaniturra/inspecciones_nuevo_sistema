f851bc24ea06cce3d05ce9e58ef4bd173db9b57a
Ivaniturra <162341297+Ivaniturra@users.noreply.github.com>
2025-10-08 21:11:50 -0300

    21.11

--- app/app/Views/auth/login.php
+++ app/app/Views/auth/login.php
@@ -174,7 +174,7 @@
           <div class="d-flex justify-content-between align-items-center mb-3">
             <div class="form-check">
               <input class="form-check-input" type="checkbox" id="remember" name="remember" <?= old('remember')?'checked':'' ?>>
-              <label class="form-check-label" for="remember">Recordarme</label>
+              <label class="form-check-label" for="remember">ss</label>
             </div>
             <a class="small" href="<?= base_url('forgot') ?>">¿Olvidaste tu contraseña?</a>
           </div>
